484b2da2e5b8c459720cfbdd29c4245d4ddd075f
dhanpalsinghrajpurohit <dhanpal.singh.rajpurohit09@gmail.com>
2022-01-04 15:16:19 +0530

    update 04-01

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save,post_delete
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -13,7 +15,6 @@ class Profile(models.Model):
     location = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True,null=True)
-
     profile_image = models.ImageField(null=True,blank=True,upload_to='profiles/',default='profiles/user-default.png')
     social_github = models.CharField(max_length=200, blank=True, null=True)
     social_stackoverflow = models.CharField(max_length=200, blank=True, null=True)
@@ -37,3 +38,4 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
